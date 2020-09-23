97faa0b1c2b5a986a68bfe629a1e18aae08f247f
Koios1143 <ken1357924681010@gmail.com>
2020-09-23 21:35:40 +0800

    update Day09

---
+++ Day09/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+import json
+
+app = Flask(__name__)
+# LINE BOT info
+line_bot_api = LineBotApi('Channel Access token')
+handler = WebhookHandler('Channel Secret')
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    print(body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+# Message event
+@handler.add(MessageEvent)
+def handle_message(event):
+    message_type = event.message.type
+    user_id = event.source.user_id
+    reply_token = event.reply_token
+    message = event.message.text
+    if(message == 'profile'):
+        FlexMessage = json.load(open('card.json','r',encoding='utf-8'))
+        line_bot_api.reply_message(reply_token, FlexSendMessage('profile',FlexMessage))
+    else:
+        line_bot_api.reply_message(reply_token, TextSendMessage(text=message))
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', port=port)
